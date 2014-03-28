b3df3a438f85a60cb650458d2a342d635d0ca121
Mahmoud Abdelkader <mabdelkader@gmail.com>
2014-03-28 14:24:15 -0700

    Default minfds to something a bit more sensible.
    we need to keep this at a sensible level since this is managing the minimum bound for all processes managed UNDER supervisor.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,5 @@
 default['supervisor']['unix_http_server']['chmod'] = "0777"
 default['supervisor']['daemon_options'] = "-c #{default['supervisor']['conffile']}"
+# we need to keep this at a sensible level since this is managing the minimum bound
+# for all processes managed UNDER supervisor.
+default['supervisor']['minfds'] = 3096
