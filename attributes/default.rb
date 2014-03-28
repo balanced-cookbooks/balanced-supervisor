1626dc130c496e6544ff2cf7ee86e1176be47553
Mahmoud Abdelkader <mabdelkader@gmail.com>
2014-03-28 14:24:41 -0700

    Update default.rb
    math

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,4 @@ default['supervisor']['unix_http_server']['chmod'] = "0777"
 default['supervisor']['daemon_options'] = "-c #{default['supervisor']['conffile']}"
 # we need to keep this at a sensible level since this is managing the minimum bound
 # for all processes managed UNDER supervisor.
-default['supervisor']['minfds'] = 3096
+default['supervisor']['minfds'] = 3072
