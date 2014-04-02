528e58e989eb05b41700ba99d1379868ee0d76c7
bninja <andrew@poundpay.com>
2014-04-01 20:09:14 -0700

    release

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dev@balancedpayments.com'
 license          'MIT'
 description      'Installs/Configures supervisor for Balanced'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.2'
 
 depends 'supervisor', '~> 0.4.0'
