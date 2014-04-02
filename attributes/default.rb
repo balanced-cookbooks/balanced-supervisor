4e78ac685c6d10f96569f771f382b2d706aff14a
bninja <andrew@poundpay.com>
2014-04-01 20:49:48 -0700

    enable 3.0 conf

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,4 +2,5 @@ default['supervisor']['unix_http_server']['chmod'] = "0777"
 default['supervisor']['daemon_options'] = "-c #{default['supervisor']['conffile']}"
 # we need to keep this at a sensible level since this is managing the minimum bound
 # for all processes managed UNDER supervisor.
+default['supervisor']['version'] = '3.0'
 default['supervisor']['minfds'] = 3072
